f61c45f28c046dae53ccbf84986151c9d18ecad0
Karl <seillier.karl@gmail.com>
2014-04-08 11:59:02 +0200

    Creation de la classe Film
    la class Film n'est pas fini !

---
+++ AppliCinema/AppliCinema/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppliCinema
+{
+    class Film
+    {
+        private string affiche;
+        private string nom;
+
+        public Film (string uneAffiche,string unNom)
+        
+        {
+            this.affiche = uneAffiche;
+            this.nom = unNom;
+
+        }
+
+    }
+}
